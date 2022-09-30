4fc957c27a65ee0f41125bef898084b5825107e0
alex-na-web <nemen71@gmail.com>
2022-09-30 17:55:42 +0300

    first commit

---
+++ canvas.py
@@ -0,0 +1,20 @@
+import numpy as np
+from PIL import Image
+
+
+class Canvas:
+    """Object where all shapes will be drawn"""
+    def __init__(self, height, width, color):
+        self.color = color
+        self.height = height
+        self.width = width
+
+        # Create a 3d numpy array of zeros
+        self.data = np.zeros((self.height, self.width, 3), dtype=np.uint8)
+        # Change [0,0,0] with user given values for color
+        self.data[:] = [255, 255, 255]
+
+    def make(self, image_path):
+        """Converts the current array into an image file"""
+        img = Image.fromarray(self.data, 'RGB')
+        img.save(image_path)
